fbc390897a07bccca6ca2c3d5e96d84b28b452ae
akirahashibami <hashibami2207@gmail.com>
2020-07-06 06:37:13 +0000

    [Finish] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,16 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	@book = Book.new(book_params)
+      # index.htmlに変数を渡すので@bookとなる
+    if @book.save
+      # 新規投稿が保存された後、リダイレクト先でサクセスメッセージを表示する
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+       # renderを使う時は@bookを用いて内容を引っ張ってくること
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -23,15 +30,21 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book.id), notice: `Book was successfully updated.`
+  	@book = Book.find(params[:id])
+    if @book.update(book_params)
+      # 更新成功後、リダイレクトした先でサクセスメッセージを表示する
+  	 redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      render :edit
+    end
+
   end
 
   def destroy
   	book = Book.find(params[:id])
   	book.destroy
-  	redirect_to books_path
+    # 削除後、リダイレクト先でサクセスメッセージを表示する
+  	redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
   private

ae8765dabaf293023cf93fe7d325e4c59bbef0de
akirahashibami <hashibami2207@gmail.com>
2020-07-06 01:33:40 +0000

    [add] application.css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
-  	redirect_to books_path
+  	redirect_to book_path(book.id)
   end
 
   def show
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   def update
   	book = Book.find(params[:id])
   	book.update(book_params)
-  	redirect_to book_path(book.id)
+  	redirect_to book_path(book.id), notice: `Book was successfully updated.`
   end
 
   def destroy
